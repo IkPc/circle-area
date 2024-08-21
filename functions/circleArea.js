205af5c44ee12d7107857f00083e3c0272672b6b
Pabllo Cristian <pabllo.dev@gmail.com>
2024-08-21 01:30:27 -0300

    Initial commit

---
+++ functions/circleArea.js
@@ -0,0 +1,5 @@
+function circleArea(r) {
+    return Math.PI * r * r;   
+}
+
+module.exports = circleArea;
---
+++ tests.js
@@ -0,0 +1,5 @@
+const circleArea = require('./functions/circleArea.js');
+
+console.log(circleArea(1));
+console.log(circleArea(10));
+console.log(circleArea(100));
